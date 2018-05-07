a8e071aeff9c49f1a0c83eb6702aa86ede2bd32e
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-05-07 19:22:34 +0300

    greet user by name

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import {askName} from '..'
+
 console.log('Welcome to Brain Games!');
+console.log(`Hello, ${askName()}!`);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import readlineSync from 'readline-sync';
+
+export const askName = () => readlineSync.question('May I have your name? ' ,{
+  defaultInput: 'Anonim'});
