1d346000bd5e9351fb8c1defec5e0bd8838b3c81
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-05-08 16:58:06 +0300

    create a start function

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
-import {askName} from '..';
+import start from '..';
+
+start();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to Brain Games!');
-const askName = readlineSync.question('May I have your name? ', { defaultInput: 'Anonim' });
-console.log(`Hello, ${askName}!`);
-export default askName;
+const start = () => {
+  console.log('Welcome to Brain Games!');
+  const askName = readlineSync.question('May I have your name? ', { defaultInput: 'Anonim' });
+  console.log(`Hello, ${askName}!`);
+};
+
+export default start;
