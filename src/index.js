5748a51fffbe2aa1803fc72b3b9efbd6569e40db
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-05-09 17:41:32 +0300

    implement brain even game

--- src/index.js
+++ src/index.js
@@ -1,9 +1,53 @@
 import readlineSync from 'readline-sync';
 
-const start = () => {
+const greetUser = () => {
   console.log('Welcome to Brain Games!');
-  const askName = readlineSync.question('May I have your name? ', { defaultInput: 'Anonim' });
-  console.log(`Hello, ${askName}!`);
+};
+const showEvenGameRules = () => {
+  console.log('Answer "yes" if number even otherwise answer "no". \n');
+};
+const getUsername = () => {
+  const username = readlineSync.question('May I have your name? ', { defaultInput: 'Anonim' });
+  console.log(`Hello, ${username}!\n`);
+  return username;
 };
 
-export default start;
+const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+const isEven = num => num % 2 === 0;
+
+const toBool = (answer) => {
+  if (answer === 'yes' || answer === 'no') {
+    return answer === 'yes';
+  }
+  return undefined;
+};
+
+const resultFormating = bool => (bool ? 'yes' : 'no');
+
+const gameEven = (username, iter) => {
+  const lastGameIterationValue = 3;
+  if (iter === lastGameIterationValue) {
+    return console.log(`Congratulations, ${username}!`);
+  }
+  const maxNum = 100;
+  const randomNum = getRandomInt(maxNum);
+  console.log('Question:', randomNum);
+  const userAswer = readlineSync.question('Your answer: ');
+  const message = (`'${userAswer}' is wrong answer ;(.
+    Correct answer was '${resultFormating(isEven(randomNum))}'. Let's try again, ${username}`);
+
+  if (toBool(userAswer) === isEven(randomNum)) {
+    console.log('Correct!');
+    return gameEven(username, iter + 1);
+  }
+  console.log(message);
+  return true;
+};
+
+const runEvenGame = () => {
+  greetUser();
+  showEvenGameRules();
+  const username = getUsername();
+  return gameEven(username, 0);
+};
+export default runEvenGame;
