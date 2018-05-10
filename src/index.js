9b0ace2b1ed1e99dd74439593e13b5c1ac89cb44
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-05-10 18:21:18 +0300

    Remove games logic

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import readlineSync from 'readline-sync';
-import gameEven from './even_game';
-import gameCalc from './calc_game';
 
-const getUsername = () => {
+export const getUsername = () => {
   const username = readlineSync.question('May I have your name? ', { defaultInput: 'Anonim' });
   console.log(`Hello, ${username}!\n`);
   return username;
-};
-
-export const runEvenGame = () => {
-  console.log('Welcome to Brain Games!');
-  console.log('Answer "yes" if number even otherwise answer "no". \n');
-  const username = getUsername();
-  return gameEven(username, 0);
-};
-
-export const runCalcGame = () => {
-  console.log('Welcome to Brain Games!');
-  console.log('What is the result of the expression?. \n');
-  const username = getUsername();
-  return gameCalc(username, 0);
 };
 
 export const runGames = () => {
