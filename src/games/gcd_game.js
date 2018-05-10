1bcf31c0d0be92013dc7fcd34167e383796a0c52
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-05-10 19:52:00 +0300

    Implement brain gcd game

---
+++ src/games/gcd_game.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+import { getRandomNumber, gcd } from '../utils';
+import { getUsername } from '..';
+
+const defineRightAnswer = (num1, num2) => gcd(num1, num2);
+const maxNumber = 10;
+const lastGameLap = 3;
+
+const gameGcd = (username, lap) => {
+  if (lap === lastGameLap) {
+    console.log(`Congratulations, ${username}!`);
+    return;
+  }
+  const randomNum1 = getRandomNumber(maxNumber);
+  const randomNum2 = getRandomNumber(maxNumber);
+  const rightAnswer = defineRightAnswer(randomNum1, randomNum2).toString();
+
+  console.log('Question:', randomNum1, randomNum2);
+  const userAnswer = readlineSync.question('Your answer: ');
+
+  if (userAnswer === rightAnswer) {
+    console.log('Correct!');
+    gameGcd(username, lap + 1);
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(.
+    Correct answer was '${rightAnswer}'. Let's try again, ${username}`);
+  }
+};
+
+const runGcdGame = () => {
+  console.log('Welcome to Brain Games!');
+  console.log('Find the greatest common divisor of given numbers. \n');
+  return gameGcd(getUsername(), 0);
+};
+export default runGcdGame;
