545a0df1463ec2507f5ad887b0cd91764fb02263
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-05-10 19:52:29 +0300

    Add gcd function

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,9 @@
 export const getRandomNumber = max => Math.floor(Math.random() * Math.floor(max));
 export const isEven = num => num % 2 === 0;
+
+export const gcd = (num1, num2) => {
+  if (num2 === 0) {
+    return Math.abs(num1);
+  }
+  return gcd(num2, num1 % num2);
+};
