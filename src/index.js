2e66f1f7a253def017ae24c18f16fa96e1a27a00
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-05-11 13:23:41 +0300

    Extract game engine

--- src/index.js
+++ src/index.js
@@ -6,6 +6,30 @@ export const getUsername = () => {
   return username;
 };
 
+const lastGameLap = 3;
+
+const gameLoop = (questionFunc, username, lap) => {
+  if (lap === lastGameLap) {
+    console.log(`Congratulations, ${username}!`);
+    return;
+  }
+  const answer = questionFunc(lap);
+
+  if (answer.user === answer.right) {
+    console.log('Correct!');
+    gameLoop(questionFunc, username, lap + 1);
+  } else {
+    console.log(`'${answer.user}' is wrong answer ;(.
+    Correct answer was '${answer.right}'. Let's try again, ${username}`);
+  }
+};
+
+export const runGame = (game) => {
+  console.log('Welcome to Brain Games!');
+  console.log(game.rules);
+  return gameLoop(game.logic, getUsername(), 0);
+};
+
 export const runGames = () => {
   console.log('Welcome to Brain Games!');
   getUsername();
