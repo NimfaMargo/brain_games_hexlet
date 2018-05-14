163aaac6f8d86bc88079722484ab7723822d9b5f
Margarita Nikiforova <Nikiforova.Rita@gmail.com>
2018-05-14 17:19:00 +0300

    Delete defineRightAnswer function

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,13 +2,12 @@ import { getRandomNumber } from '../utils';
 import runGame from '..';
 
 const gcd = (num1, num2) => (num2 === 0 ? Math.abs(num1) : gcd(num2, num1 % num2));
-const defineRightAnswer = (num1, num2) => gcd(num1, num2);
 const maxNumber = 100;
 
 const logic = () => {
   const randomNum1 = getRandomNumber(maxNumber);
   const randomNum2 = getRandomNumber(maxNumber);
-  const answer = defineRightAnswer(randomNum1, randomNum2).toString();
+  const answer = gcd(randomNum1, randomNum2).toString();
   const question = `${randomNum1} ${randomNum2}`;
   return { answer, question };
 };
